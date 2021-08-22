d7c1253ffccbc3040dd34a96efa45f7e4c913fb0
IgnatevD <ignatevds@gmail.com>
2021-08-22 15:15:23 +0300

    Без правок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import Section from "./Section/Section";
 
 const App = () => {
   return (
-    <Section titel={"Отзывы о компании"}>
-      <Feedback />
-    </Section>
+      <Section titel={"Отзывы о компании"}>
+        <Feedback />
+      </Section>
   );
 };
 
